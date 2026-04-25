48962cdbae25de1478079c4f50b000a167ca8a8f
Kuat203 <jsjksolois@gmail.com>
2026-04-25 14:43:29 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/clients")
+def clients():
+    conn = get_db()
+    clients = conn.execute("SELECT * FROM clients").fetchall()
+    conn.close()
+    return render_template("clients.html", clients=clients)
+
+@app.route("/add_client", methods=["GET", "POST"])
+def add_client():
+    if request.method == "POST":
+        name = request.form["name"]
+        phone = request.form["phone"]
+
+        conn = get_db()
+        conn.execute("INSERT INTO clients (full_name, phone) VALUES (?, ?)", (name, phone))
+        conn.commit()
+        conn.close()
+
+        return redirect("/clients")
+
+    return render_template("add_client.html")
+
+if __name__ == "__main__":
+    app.run()
