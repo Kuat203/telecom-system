c07ad48a132173035f54f1667ab4f774ed6d64a1
Kuat203 <jsjksolois@gmail.com>
2026-04-26 14:58:39 +0500

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,33 +1,41 @@
-import sqlite3
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
 
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS clients (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        full_name TEXT,
+        phone TEXT
+    )
+    """)
 
-cursor.execute("""
-CREATE TABLE clients (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    full_name TEXT,
-    phone TEXT
-)
-""")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS services (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price INTEGER
+    )
+    """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS payments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    client_id INTEGER,
-    amount INTEGER,
-    date TEXT
-)
-""")
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS subscriptions (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    client_id INTEGER,
-    service_id INTEGER,
-    start_date TEXT,
-    status TEXT
-)
-""")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS subscriptions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_id INTEGER,
+        service_id INTEGER,
+        start_date TEXT,
+        status TEXT
+    )
+    """)
 
-conn.commit()
-conn.close()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS payments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_id INTEGER,
+        amount INTEGER,
+        date TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
